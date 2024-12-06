e2edc8e363046ac667539c44a7410c1341be6619
37533 <andrey.naumovets@gmail.com>
2024-12-06 21:07:13 +0300

    Deployment demo

--- src/main/java/com/sample/depolyment/deployment_project/NaumController.java
+++ src/main/java/com/sample/depolyment/deployment_project/NaumController.java
@@ -1,21 +1,32 @@
 package com.sample.depolyment.deployment_project;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/naa")
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.net.http.HttpRequest;
+
+@Controller
 public class NaumController {
 
-    @GetMapping()
-    public String hello1Page(){
-        return "Hello naa1";
-    }
-
     @GetMapping("/")
-    public String hello2Page(){
-        return "Hello naa2";
+    public String getNetworkInfo(HttpServletRequest request, Model model) {
+        try {
+            // Получаем имя хоста (название устройства)
+            String hostName = InetAddress.getLocalHost().getHostName();
+            // Получаем IP адрес устройства
+            String ipAddress = request.getRemoteAddr();
+
+            model.addAttribute("hostName", hostName);
+            model.addAttribute("ipAddress", ipAddress);
+        } catch (UnknownHostException e) {
+            model.addAttribute("hostName", "Не удалось получить имя устройства");
+            model.addAttribute("ipAddress", "Не удалось получить IP адрес");
+        }
+        return "index";
     }
 }
